39a920424261859823d68fc1af39c6dec66ff5f2
zhang.j.k <zhangjk67@gmail.com>
2021-03-29 19:11:53 +0800

    change url

--- utils/util.go
+++ utils/util.go
@@ -130,7 +130,7 @@ func SendMail(conf *conf.SmtpConf, subject, email string, body string) error {
 //渲染markdown为html并录入数据库
 func RenderDocumentById(id int) {
 	//使用chromium-browser
-	//	chromium-browser --headless --disable-gpu --screenshot --no-sandbox --window-size=320,480 http://www.bookstack.cn
+	//	chromium-browser --headless --disable-gpu --screenshot --no-sandbox --window-size=320,480 http://localhost
 	link := "http://localhost:" + beego.AppConfig.DefaultString("httpport", "8181") + "/local-render?id=" + strconv.Itoa(id)
 	name := beego.AppConfig.DefaultString("chrome", "chromium-browser")
 	args := []string{"--headless", "--disable-gpu", "--screenshot", "--no-sandbox", "--window-size=320,480", link}
@@ -203,7 +203,7 @@ func RenderCoverByBookIdentify(identify string) (err error) {
 	}
 
 	//使用chromium-browser
-	//	chromium-browser --headless --disable-gpu --screenshot --no-sandbox --window-size=320,480 http://www.bookstack.cn
+	//	chromium-browser --headless --disable-gpu --screenshot --no-sandbox --window-size=320,480 http://localhost
 	link := "http://localhost:" + beego.AppConfig.DefaultString("httpport", "8181") + "/local-render-cover?id=" + identify
 	name := "node"
 	if ok := beego.AppConfig.DefaultBool("puppeteer", false); ok {
