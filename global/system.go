ef6771c1328269dc014579382d75f73f4d83fc24
zhang.j.k <zhangjk67@gmail.com>
2021-04-19 20:29:58 +0800

    update and edit build.sh

--- global/system.go
+++ global/system.go
@@ -1,6 +1,6 @@
 package global
 
 const (
-	SYSTEM_VERSION   = "v0.2.1"              // system version code
-	SYSTEM_COPYRIGHT = "2018 - 2020 phachon" // system copyright
+	SYSTEM_VERSION   = "v0.0.1"            // system version code
+	SYSTEM_COPYRIGHT = "2021 - 至今 数字健康实验室" // system copyright
 )
