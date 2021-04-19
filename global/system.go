a01d1a7a893b5ee8f9a97f6edf840eac91522a30
zhang.j.k <zhangjk67@gmail.com>
2021-04-19 21:08:31 +0800

    modify some info

--- global/system.go
+++ global/system.go
@@ -1,6 +1,6 @@
 package global
 
 const (
-	SYSTEM_VERSION   = "v0.0.1"            // system version code
-	SYSTEM_COPYRIGHT = "2021 - 至今 数字健康实验室" // system copyright
+	SYSTEM_VERSION   = "v0.0.1"              // system version code
+	SYSTEM_COPYRIGHT = "2020 - 2021 数字健康实验室" // system copyright
 )
